c77d25c372b22dc0d2d3ccde40c9a72bcae1874c
Mojtaba Kiani <kiani@live.com>
2020-11-01 20:23:55 +0330

    Update Migrate after QuestionAnswer

---
+++ src/GRC.Infrastructure/Migrations/20201101164959_AfterQuestionAnswer.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GRC.Infrastructure.Migrations
+{
+    public partial class AfterQuestionAnswer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnswersList",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "CorrectAnswerIndex",
+                table: "Questions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnerUid",
+                table: "Questionaries",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "QuestionAnswers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AnswerText = table.Column<string>(nullable: false),
+                    Value = table.Column<double>(nullable: false),
+                    IsCorrectAnswer = table.Column<bool>(nullable: false),
+                    QuestionId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionAnswers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QuestionAnswers_Questions_QuestionId",
+                        column: x => x.QuestionId,
+                        principalTable: "Questions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionAnswers_QuestionId",
+                table: "QuestionAnswers",
+                column: "QuestionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QuestionAnswers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AnswersList",
+                table: "Questions",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CorrectAnswerIndex",
+                table: "Questions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnerUid",
+                table: "Questionaries",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
