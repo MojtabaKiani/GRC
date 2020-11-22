fe1fc26cbb3e1b7e3e551e2cb81e0349e0af9173
Mojtaba Kiani <kiani@live.com>
2020-11-22 21:13:30 +0330

    Add GRCIdentityProviderContext

---
+++ src/GRC.IdentityProvider.Infrastructure/GRCIdentityProviderContext.cs
@@ -0,0 +1,20 @@
+﻿using GRC.Core.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GRC.IdentityProvider.Infrastructure
+{
+    public class GRCIdentityProviderContext : IdentityDbContext<GRCUser,IdentityRole,string>
+    {
+        public GRCIdentityProviderContext(DbContextOptions<GRCIdentityProviderContext> options):base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
