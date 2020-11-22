17c63b686c7a528e4f970f998fbc1a34705ffacb
Mojtaba Kiani <kiani@live.com>
2020-11-22 20:27:54 +0330

    Add GRCUser Class

---
+++ src/GRC.CORE/Identity/GRCUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GRC.Core.Identity
+{
+    public class GRCUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Organization { get; set; }
+
+        [Display(Name = "Full Name")]
+        [NotMapped]
+        public string FullName => $"{Name} {LastName} - {Email}";
+
+    }
+}
