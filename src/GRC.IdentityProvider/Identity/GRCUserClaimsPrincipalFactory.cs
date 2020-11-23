f848b65fb2fb7d594acce232714377c2eca4f916
Mojtaba Kiani <kiani@live.com>
2020-11-23 14:18:39 +0330

    Add Views & Controls to IdentityProvider

---
+++ src/GRC.IdentityProvider/Identity/GRCUserClaimsPrincipalFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using GRC.Core.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace GRC.IdentityProvider.Areas.Identity
+{
+    public class GRCUserClaimsPrincipalFactory:
+        UserClaimsPrincipalFactory<GRCUser, IdentityRole>
+    {
+        public GRCUserClaimsPrincipalFactory(UserManager<GRCUser> userManager,RoleManager<IdentityRole> roleManager,IOptions<IdentityOptions> options)
+            : base (userManager, roleManager, options)
+        {
+        }
+
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(GRCUser user)
+        {
+            var identity = await base.GenerateClaimsAsync(user);
+            identity.AddClaim(new Claim("Organization", user.Organization));
+            identity.AddClaim(new Claim("FullName", user.FullName));
+            return identity;
+        }
+    }
+}
