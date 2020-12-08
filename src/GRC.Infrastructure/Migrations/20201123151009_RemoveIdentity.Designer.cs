4df767c2d5fef0c1f5a1c22bf75f88c2b5fe6802
Mojtaba Kiani <kiani@live.com>
2020-12-08 13:35:42 +0330

    Add migrate after GRCContext changed

--- src/GRC.Infrastructure/Migrations/20201123151009_RemoveIdentity.Designer.cs
+++ src/GRC.Infrastructure/Migrations/20201123151009_RemoveIdentity.Designer.cs
@@ -4,14 +4,16 @@ using GRC.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GRC.Infrastructure.Migrations
 {
     [DbContext(typeof(GRCContext))]
-    partial class GRCContextModelSnapshot : ModelSnapshot
+    [Migration("20201123151009_RemoveIdentity")]
+    partial class RemoveIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
